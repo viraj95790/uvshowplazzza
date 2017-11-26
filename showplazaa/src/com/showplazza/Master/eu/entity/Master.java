410b7adb6f8bea7a48ba40583960f92c73f94e28
acer <acer@192.168.0.3>
2017-11-26 20:05:55 +0530

    m

--- showplazaa/src/com/showplazza/Master/eu/entity/Master.java
+++ showplazaa/src/com/showplazza/Master/eu/entity/Master.java
@@ -61,14 +61,63 @@ public class Master {
 	private String movieImageFileName2;
 	
 	
+	private String ticketname;
 	
+	private String quantity;
 	
+	private String clas_type;
+	
+	private String ticket_des;
+	
+	private String message;
 	
 	
 	
 
 	
 	
+	
+	
+
+	public String getTicketname() {
+		return ticketname;
+	}
+
+	public void setTicketname(String ticketname) {
+		this.ticketname = ticketname;
+	}
+
+	public String getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getClas_type() {
+		return clas_type;
+	}
+
+	public void setClas_type(String clas_type) {
+		this.clas_type = clas_type;
+	}
+
+	public String getTicket_des() {
+		return ticket_des;
+	}
+
+	public void setTicket_des(String ticket_des) {
+		this.ticket_des = ticket_des;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
 
 	public String getCity() {
 		return city;
